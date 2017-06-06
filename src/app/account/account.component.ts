0d25a66173ea1552121aea973aa592a9845f6a65
Clyde Mendonca <clyde.mendonca@tripin.co.in>
2017-06-06 09:55:48 +0530

    Calling a service from a service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import { AccountsService } from '../accounts.service'
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [ LoggingService ]
+  // providers: [ LoggingService ]
 })
 export class AccountComponent {
 
@@ -18,7 +18,8 @@ export class AccountComponent {
   }
 
   onSetTo(status: string) {
-    this.loggingService.logStatusChange(status);
+    // this.loggingService.logStatusChange(status);
     this.accountsService.updateStatus(this.id, status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
